60321046d1e12fda0d9ea64251fb89b4504ea7d7
Briseid Pagador <bpagador@gmail.com>
2020-04-06 15:54:54 -0700

    show product data

---
+++ data/wines.js
@@ -0,0 +1,77 @@
+const beaujolais = {
+    id: 'beaujolais',
+    name: 'Beaujolais',
+    image: 'beaujolais.jpg',
+    description: 'The grape of Beaujolais, Gamay is often made to be drunk quite young, and shows bright, tangy, fruit-driven flavors of strawberry, raspberry, and sweet cherries. When made by the method known as carbonic maceration, young Gamay has a slight effervescence and a distinct smell of bananas. Beaujolais Nouveau, released each year shortly after harvest, is the most famous example.',
+    category: 'red',
+    price: 21.00,
+};
+const cabSauv = {
+    id: 'cab-sauv',
+    name: 'Cabernet Sauvignon',
+    image: 'cabsauv.jpg',
+    description: 'The primary component of great Bordeaux and the defining grape of the Napa Valley, Cabernet Sauvignon is grown all over the world, but rarely achieves greatness. It ripens late and can be quite weedy and even vegetal in cooler climate regions such as Chile. In Bordeaux and Tuscany it is almost always blended to soften its intensely astringent tannins. The Napa style is dense, purple-black, jammy and tasting of currants and black cherries. Thick and ripe, layered with expensive new oak scents and flavors, it has almost single-handedly created the phenomenon of the cult wineries. In Washington, the best Cabernet straddles the border between the ripeness of California versions and the nuanced herb, leaf, and olive flavors of great Bordeaux.',
+    category: 'red',
+    price: 15.00,
+};
+const nebbiolo = {
+    id: 'nebbiolo',
+    name: 'Nebbiolo',
+    image: 'nebbiolo.jpg',
+    description: 'The principal grape of Barolo, Barbaresco, and Gattinara (all made in the Piedmont region of Italy), Nebbiolo unquestionably belongs with the great red wines of the world, but has proven almost impossible to grow anywhere else. California versions, despite decades of effort, remain light, thin and generic.',
+    category: 'red',
+    price: 17.00,
+};
+const sangiovese = {
+    id: 'sangiovese',
+    name: 'Sangiovese',
+    image: 'sangiovese.jpg',
+    description: 'The principal grape of Tuscany, where it is the primary component of Chianti and Brunello di Montalcino. Sangiovese is relatively light in color and quite firmly acidic. In Italy it shows distinctive flavors of pie cherry, anise, and tobacco; elsewhere it can be rather plain and undistinguished, though some promising bottles have come from Washington’s Walla Walla Valley. Many of Italy’s “Super Tuscan”  red blends marry Sangiovese to Cabernet Sauvignon, a combination that both strengthens the Sangiovese and smooths out the Cabernet.',
+    category: 'red',
+    price: 18.00,
+};
+const chardonnay = {
+    id: 'chardonnay',
+    name: 'Chardonnay',
+    image: 'chardonnay.jpg',
+    description: 'Chardonnay is unusual because of its versatility; it can be grown in cooler climates or warmer climates. Cool climate Chardonnay can result in notes of green apples, green pears, citrus, and vegetable notes such as cucumber; white stone fruit and melon may also be present in moderate climates; tropical fruit notes such as peach, banana, pineapple, mango, and even fig can be found in warmer climate Chardonnay. In addition to climate, the style of Chardonnay varies depending on three important factors determined by the winemaker: malolactic fermentation, time spent on the lees, and oak treatment; adding some level of buttery, nutty, coconut, and oak flavors the wine.',
+    category: 'white',
+    price: 12.00,
+};
+const pinotGrigio = {
+    id: 'pinot-grigio',
+    name: 'Pinot Grigio',
+    image: 'pinotgrigio.jpg',
+    description: 'These are two names for the same grape variety. However, Pinot Gris from Alsace, France is a full-body, dry to off-dry wine with vibrant notes of pineapple, melon, ginger, baking spices, honey and smoky; it is complex yet still crisp and refreshing. Pinot Grigio from Italy offers very little similarity to Pinot Gris from Alsace. Pinot Grigio is typically a dry, light to medium body wine with medium acidity; it is almost neutral in aromas and flavors. Large producers have diluted the quality perception of Pinot Grigio, however if you buy from boutique wineries, it can be a lovely and complex white wine.',
+    category: 'white',
+    price: 14.00,
+};
+const riesling = {
+    id: 'riesling',
+    name: 'Riesling',
+    image: 'riesling.jpg',
+    description: 'Riesling is an aromatic white grape with high acidity; it is fruity and floral rather than herbaceous; in cool climates it offers notes of green apple, green pear, floral notes, and citrus (lemon, lime); in moderate climates citrus and stone fruit become dominant, along with notes of lime and white peach; Riesling ages well, notes of smoke and petrol develop over time.',
+    category: 'white',
+    price: 16.00,
+};
+const sauvBlanc = {
+    id: 'sauv-blanc',
+    name: 'Sauvignon Blanc ',
+    image: 'sauvblanc.jpg',
+    description: 'Sauvignon Blanc is an aromatic grape variety that typically displays strong aromas of green fruit, gooseberry, elderflower, green bell pepper, grapefruit, passion fruit, and asparagus; typically high in acidity, medium body, and almost always dry; requires a cool climate to showcase its herbaceous qualities but can tolerate a moderate climate.',
+    category: 'white',
+    price: 11.00,
+};
+
+const wines = {
+    beaujolais,
+    cabSauv,
+    nebbiolo,
+    sangiovese, 
+    chardonnay,
+    pinotGrigio,
+    riesling,
+    sauvBlanc,
+};
+
+export default wines;
