60a1211d460ae05bb967a233808e6a2163c231df
Briseid Pagador <bpagador@gmail.com>
2020-04-07 15:28:18 -0700

    TDD findById function

---
+++ common/utils.js
@@ -0,0 +1,9 @@
+export function findById(items, id) {
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        if (item.id === id) {
+            return item;
+        }
+    }
+    return null;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,31 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import wines from '../data/wines.js';
+import { findById } from '../common/utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('find product by id', assert => {
     //Arrange
     // Set up your parameters and expectations
+    const id = 'beaujolais';
+    const expected = 'Beaujolais';
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const foundWine = findById(wines, id);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.ok(foundWine);
+    assert.equal(foundWine.name, expected);
+});
+
+test('find product by id', assert => {
+    const id = 'not found';
+    const expected = null;
+
+    const foundWine = findById(wines, id);
+
+    assert.equal(foundWine, expected);
+
 });
